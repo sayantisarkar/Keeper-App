098f8fd82ccde9f38b33216e595bd9f44db68457
sayantisarkar <sayantisarkar088@gmail.com>
2021-03-22 16:59:12 +0400

    Challenge 1 completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const date = new Date();
+const year = date.getFullYear();
+console.log(year);
+
+function footer(){
+    return (
+        <footer>
+        <p>Copyright © {year}</p>
+     </footer>
+    );
+}
+
+export default footer;
--- src/index.js
+++ src/index.js
@@ -14,10 +14,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App";
 
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <App />,
   document.getElementById('root')
 );
 
